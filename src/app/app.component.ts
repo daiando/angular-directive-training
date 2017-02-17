3e3c8213f26f781cdf8d672b1c8dbf840b26527f
“Dai <dai.ando@rakuten.com>
2017-02-17 11:24:25 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+import { HighlightDirective } from './highlight.directive';
+
 @Component({
   selector: 'dir-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dir works!';
 }
