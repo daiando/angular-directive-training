0be58aad4887d307b68d31f7bd738faa40c959a9
“Dai <dai.ando@rakuten.com>
2017-02-17 17:31:42 +0900

    unless directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HighlightDirective } from './highlight.directive';
+import { UnlessDirective } from './unless.directive';
 
 @Component({
   selector: 'dir-root',
@@ -8,4 +9,10 @@ import { HighlightDirective } from './highlight.directive';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private switchtest = true;
+  private items = [ 1, 2, 3, 4, 5, 6 ];
+  private testval = 10;
+  onSwitchTtest() {
+    this.switchtest = !this.switchtest;
+  }
 }
